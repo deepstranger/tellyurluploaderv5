e421b2fde2aaca724bd780124679245fd8158000
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2020-09-05 00:46:19 +0530

    Update display_progress.py

--- helper_funcs/display_progress.py
+++ helper_funcs/display_progress.py
@@ -33,7 +33,7 @@ async def progress_for_pyrogram(
     diff = now - start
     if round(diff % 10.00) == 0 or current == total:
         # if round(current / total * 100, 0) % 5 == 0:
-        Uploading = current * 100 / total
+        percentage = current * 100 / total
         speed = current / diff
         elapsed_time = round(diff) * 1000
         time_to_completion = round((total - current) / speed) * 1000
@@ -43,15 +43,14 @@ async def progress_for_pyrogram(
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
         progress = "[{0}{1}] \nP: {2}%\n".format(
-           round(Uploading, 2),
-          
             ''.join(["█" for i in range(math.floor(percentage / 5))]),
-            ''.join(["░" for i in range(20 - math.floor(percentage / 5))]))
+            ''.join(["░" for i in range(20 - math.floor(percentage / 5))]),
+            round(percentage, 2))
 
-        tmp = progress + "{0}\n 📥Downloaded... {1}\n🐌Speed: {2}/s\nETA: {3}\n".format(
-            humanbytes(📥current),
+        tmp = progress + "{0} of {1}\nSpeed: {2}/s\nETA: {3}\n".format(
+            humanbytes(current),
             humanbytes(total),
-            humanbytes(🐌speed),
+            humanbytes(speed),
             # elapsed_time if elapsed_time != '' else "0 s",
             estimated_total_time if estimated_total_time != '' else "0 s"
         )
