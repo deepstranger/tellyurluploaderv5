1a565790ddb736012f042fd4087dd6049446164a
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-01-04 18:07:50 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -219,12 +219,12 @@ async def echo(bot, update):
             format_id = response_json["format_id"]
             format_ext = response_json["ext"]
             cb_string_file = "{}|{}|{}".format(
-                "📁file", format_id, format_ext)
+                "file", format_id, format_ext)
             cb_string_video = "{}|{}|{}".format(
                 "video", format_id, format_ext)
             inline_keyboard.append([
                 pyrogram.InlineKeyboardButton(
-                    "SVideo",
+                    "🎞SVideo",
                     callback_data=(cb_string_video).encode("UTF-8")
                 ),
                 pyrogram.InlineKeyboardButton(
