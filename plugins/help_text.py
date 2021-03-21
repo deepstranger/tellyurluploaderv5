6811ff520ae2b2c9761a7793e149b90cd829cada
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-03-21 23:35:10 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -31,24 +31,23 @@ def GetExpiryDate(chat_id):
     return expires_at
 
 
-@pyrogram.Client.on_message(pyrogram.filters.command(["help"]))
+@pyrogram.Client.on_message(pyrogram.filters.command(["help", "about"]))
 async def help_user(bot, update):
     # logger.info(update)
     TRChatBase(update.from_user.id, update.text, "/help")
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.HELP_USER,
-      reply_markup=pyrogram.InlineKeyboardMarkup([[InlineKeyboardButton(text="⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/All_Movie_Rockers")]]),
         parse_mode="html",
         disable_web_page_preview=True,
         reply_to_message_id=update.message_id
     )
 
 
-@pyrogram.Client.on_message(pyrogram.filters.command(["abour"]))
+@pyrogram.Client.on_message(pyrogram.filters.command(["me"]))
 async def get_me_info(bot, update):
     # logger.info(update)
-    TRChatBase(update.from_user.id, update.text, "/about")
+    TRChatBase(update.from_user.id, update.text, "/me")
     chat_id = str(update.from_user.id)
     chat_id, plan_type, expires_at = GetExpiryDate(chat_id)
     await bot.send_message(
@@ -67,10 +66,6 @@ async def start(bot, update):
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.START_TEXT,
-      reply_markup=pyrogram.InlineKeyboardMarkup([[InlineKeyboardButton(text="⭕️ CHANNEL ⭕️", url="https://t.me/All_Movie_Rockers")], [InlineKeyboardButton(text="😇 SUPPORT", url="https://t.me/allmovierockerssdiscussion"),
-                                                    InlineKeyboardButton(text="Creator ♐️", url="https://t.me/shreevish")]]),
-      parse_mode="html",
-        disable_web_page_preview=True,
         reply_to_message_id=update.message_id
     )
 
