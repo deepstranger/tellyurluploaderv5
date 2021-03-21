55461c77626f463bb8c6d56a4285aa7c2576a58f
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-03-21 10:53:50 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -21,7 +21,7 @@ else:
     from config import Config
 
 # the Strings used for this "thing"
-from translation import Translation
+"""from translation import Translation"""
 
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
