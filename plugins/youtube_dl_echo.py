d4639db2ef7b4213970e218505e4829b796c1729
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-06-05 14:44:27 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -36,19 +36,10 @@ from pyrogram.errors import UserNotParticipant, UserBannedInChannel
 
 @pyrogram.Client.on_message(pyrogram.filters.regex(pattern=".*http.*"))
 async def echo(bot, update):
-    if update.from_user.id in Config.AUTH_USERS:
-        await bot.delete_messages(
-            chat_id=update.chat.id,
-            message_ids=update.message_id,
-            revoke=True
-        )
+    if update.from_user.id in Config.BANNED_USERS:
+        await update.reply_text("You are B A N N E D 🤣🤣🤣🤣")
         return
-    # logger.info(update)
     TRChatBase(update.from_user.id, update.text, "/echo")
-    # await bot.send_chat_action(
-    #     chat_id=update.chat.id,
-    #     action="typing"
-    # )
     update_channel = Config.UPDATE_CHANNEL
     if update_channel:
         try:
