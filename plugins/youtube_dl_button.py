ba4bbc513353f7986431823c44cb6fc22c0bac43
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-06-05 23:23:12 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -149,7 +149,10 @@ async def youtube_dl_call_back(bot, update):
         command_to_exec.append(youtube_dl_password)
     command_to_exec.append("--no-warnings")
     # command_to_exec.append("--quiet")
-    logger.info(command_to_exec)
+      if "hotstar" in youtube_dl_url:
+        command_to_exec.append("--geo-bypass-country")
+        command_to_exec.append("IN")
+        
     start = datetime.now()
     process = await asyncio.create_subprocess_exec(
         *command_to_exec,
